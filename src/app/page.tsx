05e1b8b03f61cf5e90153034ee5d91c7551139b1
Narcizo Palioto <narcizo.gabriel2@gmail.com>
2024-11-02 11:24:46 -0300

    feat: getting started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,82 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from 'react';
+import { asciiArts } from '../static/asciiArts';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [asciiIndex, setAasciiIndex] = useState(Math.floor(Math.random() * asciiArts.length));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAasciiIndex(Math.floor(Math.random() * asciiArts.length));
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      {/* <div className="flex flex-col justify-center items-center flex-wrap gap-3"> */}
+      <div className="grid grid-cols-1">
+        {/* <div className='absolute inset-y-14'> */}
+        <div className='grid grid-cols-subgrid text-center h-[50vh]'>
+          <div className='flex justify-center items-center'>
+            <pre className="whitespace-pre-wrap">
+              {asciiArts[asciiIndex]}
+            </pre>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* <div className='absolute inset-y-2/4'> */}
+        <div className='grid grid-cols-subgrid col-span-4'>
+          <form className="flex flex-row flex-wrap justify-center w-screen px-80">
+            <div className="flex flex-row w-full">
+              <input className="shadow appearance-none border w-full rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="url" pattern="https://.*" required placeholder="Paste the youtube link here"/>
+              <select id="format" className="w-20 justify-end items-end bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option value="mp3">mp3</option>
+                <option value="mp4">mp4</option>
+              </select>
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold my-8 py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Download
+            </button>
+          </form>
+        </div>
+      </div>
+      {/* <div className="grid grid-cols-4">
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div className='grid grid-cols-subgrid gap-4 col-span-3'>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+        <div>
+          1
+        </div>
+      </div> */}
+    </>
   );
 }
